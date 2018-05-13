5337d4cd34c7bebf033e9351717fb6a2c2d569b4
ArceusMatt <arceusthegod303@yahoo.com>
2018-05-13 14:34:58 -0400

    Create showMenuBtn.js

---
+++ Projects/showMenuBtn.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*
+Around 2016 or so me and godsoft on hangouts
+discussed a major problem with mod menus
+and that was if you tap the main button twice it
+would show 2 menus & #1 wouldn't disappear.
+so i told godsoft some key-pointers & he came up with this idea.
+*/
+
+/*
+func showMenuBtn()
+made by Godsoft029 & ArceusMatt
+Allows you to show popup after it's been dismissed.
+*/
+function showMenuBtn(){
+//your layout here
+//your button here (btn)
+//your popup variable here (gui)
+
+/*
+inside btn onclick we'd dismiss gui then
+call showMenuBtn() whenever we wanted the button to come back.
+this solution would fix the menu being stuck on the screen.
+*/
+}
